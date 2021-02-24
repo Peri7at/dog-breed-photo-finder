4c8980794be52712f51fb7d67a7a108c2177d118
John Jago <johnjago@protonmail.com>
2021-02-24 14:55:33 -0600

    Create a starting point for the assessment

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # dogs
 
+This project is a Node.js web server using the Koa framework. There is also a
+plain web page, located in `/web`, which will be used for the output.
+
+Relevant documentation:
+
+- Koa: https://koajs.com/
+- @koa/router: https://github.com/koajs/router#koarouter
+- koa-body: https://github.com/dlau/koa-body#usage-with-koa-router
+- Dogs API: https://dog.ceo/dog-api/breeds-list
+- Cat fact API: https://catfact.ninja/
+
+## Getting started
+
+To run the server, you need [**Node.js >= 14**](https://nodejs.org/) installed.
+
+You can check your current version of Node.js like this:
+
+```sh
+$ node --version
+v14.16.0
+```
+
+If you have Node 14 or above, you can then run the following.
+
+```sh
+npm install
+node index.js
+```
+
+If the server starts successfully, you will see this:
+
+```sh
+Server running on http://localhost:3011
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+import Koa from 'koa';
+import Router from '@koa/router';
+import cors from '@koa/cors';
+
+// Commented out so it doesn't get auto-removed by text editors. You may uncomment if needed.
+// import fetch from 'isomorphic-fetch';
+
+// Used for reading incoming POST bodies. Commented out for same reason as above.
+// See https://github.com/dlau/koa-body#usage-with-koa-router
+// import koaBody from 'koa-body';
+
+const app = new Koa();
+const router = new Router();
+const port = 3011;
+
+app.use(cors({origin: '*'}));
+
+router.get('/', (ctx) => {
+	ctx.body = 'hello!';
+});
+
+app.use(async (ctx, next) => {
+	await next();
+	const rt = ctx.response.get('X-Response-Time');
+	console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+app.use(async (ctx, next) => {
+	const start = Date.now();
+	await next();
+	const ms = Date.now() - start;
+	ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(router.routes());
+
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
