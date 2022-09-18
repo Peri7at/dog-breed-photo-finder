3c1a02355f5b05e51dae4eff41651aabc05746bf
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-09-18 18:51:27 +0300

    add error handlers

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -3,13 +3,22 @@ import fetch from "isomorphic-fetch";
 export async function getDogBreedPics(ctx, next) {
   try {
     let breed = ctx.query.breed;
+    if (breed == "") {
+      ctx.throw(500, "Please enter a dog breed");
+    }
     const apiResponse = await fetch(
       "https://dog.ceo/api/breed/" + breed + "/images"
     );
     const apiResponseJson = await apiResponse.json();
+    if (apiResponseJson.status == "error") {
+      ctx.throw(404, "Breed not found");
+    }
     let pictures = apiResponseJson.message;
     await ctx.render("pictures", { pictures });
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    ctx.status = err.statusCode || err.status || 500;
+    ctx.body = {
+      message: err.message,
+    };
   }
 }
